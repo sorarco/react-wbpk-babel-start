e25f1ddec1c838fd2c5cb14e186517ff1061929d
rodrigo s. arcova <rodrigo.arcova@rodrigos-MacBook-Pro.local>
2019-07-20 21:54:08 -0300

    add @coreui/coreui - para suporte ao sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import "./App.scss";
 // Styles
 // CoreUI Icons Set
-import '@coreui/icons/css/coreui-icons.min.css';
+import "@coreui/icons/css/coreui-icons.min.css";
 // Import Flag Icons Set
 // import 'flag-icon-css/css/flag-icon.min.css';
 // Import Font Awesome Icons Set
@@ -11,11 +11,11 @@ import '@coreui/icons/css/coreui-icons.min.css';
 // Import Simple Line Icons Set
 // import 'simple-line-icons/css/simple-line-icons.css';
 // Import Main styles for this application
-import './scss/style.css'
+// import "./scss/style.css";
 // import '../node_modules/@coreui/styles/scss/_dropdown-menu-right.scss';
 
 // Containers
-import Full from './containers/DefaultLayout/DefaultLayout.js';
+import Full from "./containers/DefaultLayout/DefaultLayout.js";
 
 // import { renderRoutes } from 'react-router-config';
 
